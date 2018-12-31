7f666f37d5796f3c3379f35b549d1a4a8c470664
shavit <shavit@users.noreply.github.com>
2018-12-30 21:15:52 -0500

    Handle play event

--- server/lib/livestream/router.ex
+++ server/lib/livestream/router.ex
@@ -4,11 +4,14 @@ defmodule Livestream.Router do
   plug :match
   plug :dispatch
 
-  get "/on_publish", do: on_publish(conn)
+  post "/on_publish", do: on_publish(conn)
   def on_publish(conn), do: send_resp(conn, 200, "ok")
 
-  get "/on_publish_done", do: on_publish(conn)
+  post "/on_publish_done", do: on_publish(conn)
   def on_publish_done(conn), do: send_resp(conn, 200, "ok")
+
+  post "/on_play", do: send_resp(conn, 400, "bad request")
+  post "/on_play_done", do: send_resp(conn, 400, "bad request")
 
   match _, do: send_resp(conn, 404, "404")
 end
--- server/test/livestream/router_test.exs
+++ server/test/livestream/router_test.exs
@@ -8,13 +8,32 @@ defmodule Livestream.RouterTest do
     resp = Router.on_publish conn
     assert resp.status == 200
     assert resp.resp_body == "ok"
-  end
 
-  test "on_publish_done/1 valid response" do
-    conn = Plug.Adapters.Test.Conn.conn(%Plug.Conn{private: %{}}, :post, "/on_publish_done", nil)
-    resp = Router.on_publish conn
+    resp = Router.call conn, nil
     assert resp.status == 200
     assert resp.resp_body == "ok"
   end
 
+  test "on_publish_done/1 valid response" do
+    conn = Plug.Adapters.Test.Conn.conn(%Plug.Conn{private: %{}}, :post, "/on_publish_done", nil)
+    resp = Router.on_publish_done conn
+    assert resp.status == 200
+    assert resp.resp_body == "ok"
+
+    resp = Router.call conn, nil
+    assert resp.status == 200
+    assert resp.resp_body == "ok"
+  end
+
+  test "on_play/1 is not allowed" do
+    conn = Plug.Adapters.Test.Conn.conn(%Plug.Conn{private: %{}}, :post, "/on_play", nil)
+    resp = Router.call conn, nil
+    assert resp.status == 400
+  end
+
+  test "on_play_done/1 is not allowed" do
+    conn = Plug.Adapters.Test.Conn.conn(%Plug.Conn{private: %{}}, :post, "/on_play_done", nil)
+    resp = Router.call conn, nil
+    assert resp.status == 400
+  end
 end
