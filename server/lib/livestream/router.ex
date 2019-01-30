25dcfeebf4994f9ee766feb6400e9f2ef83481e6
shavit <shavit@users.noreply.github.com>
2019-01-30 15:40:08 -0500

    Room ID lookup

--- server/lib/livestream/router.ex
+++ server/lib/livestream/router.ex
@@ -5,9 +5,22 @@ defmodule Livestream.Router do
   plug :dispatch
 
   post "/on_publish", do: on_publish(conn)
-  def on_publish(conn), do: send_resp(conn, 200, "ok")
+  def on_publish(conn) do
+    case conn do
+      %{params: %{"room_id" => room_id}} ->
+        on_publish_match_room(conn, room_id)
+      _ -> send_resp(conn, 403, "not authorized")
+    end
+  end
 
-  post "/on_publish_done", do: on_publish(conn)
+  defp on_publish_match_room(conn, room_id) do
+    case :ets.match(:livestream_state, {:rooms, [{room_id, :"$2", :"$3"}]}) do
+      [[:active, _room_state]] -> send_resp(conn, 200, "ok")
+      _ -> send_resp(conn, 403, "not authorized")
+    end
+  end
+
+  post "/on_publish_done", do: on_publish_done(conn)
   def on_publish_done(conn), do: send_resp(conn, 200, "ok")
 
   post "/on_play", do: send_resp(conn, 400, "bad request")
--- server/test/livestream/router_test.exs
+++ server/test/livestream/router_test.exs
@@ -6,12 +6,28 @@ defmodule Livestream.RouterTest do
   test "on_publish/1 valid response" do
     conn = Plug.Adapters.Test.Conn.conn(%Plug.Conn{private: %{}}, :post, "/on_publish", nil)
     resp = Router.on_publish conn
+    assert resp.status == 403
+    assert resp.resp_body == "not authorized"
+
+    resp = Router.call conn, nil
+    assert resp.status == 403
+    assert resp.resp_body == "not authorized"
+  end
+
+  test "on_publish/1 loopkup for available rooms" do
+    :ets.insert(:livestream_state, {:rooms, [{44, :active, {}}]})
+
+    args = %{room_id: 44}
+    conn = Plug.Adapters.Test.Conn.conn(%Plug.Conn{private: %{}}, :post, "/on_publish", args)
+    resp = Router.on_publish conn
     assert resp.status == 200
     assert resp.resp_body == "ok"
 
-    resp = Router.call conn, nil
-    assert resp.status == 200
-    assert resp.resp_body == "ok"
+    :ets.insert(:livestream_state, {:rooms, [{43, :active, {}}]})
+    conn = Plug.Adapters.Test.Conn.conn(%Plug.Conn{private: %{}}, :post, "/on_publish", args)
+    resp = Router.on_publish conn
+    assert resp.status == 403
+    assert resp.resp_body == "not authorized"
   end
 
   test "on_publish_done/1 valid response" do
