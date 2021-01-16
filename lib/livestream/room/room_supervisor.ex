2009c1386e0d6b880b6dd40559100a8db82866f4
shavit <shavit@users.noreply.github.com>
2021-01-16 00:10:48 -0500

    Update the supervisor

--- lib/livestream/room/room_supervisor.ex
+++ lib/livestream/room/room_supervisor.ex
@@ -1,12 +1,29 @@
 defmodule Livestream.Room.RoomSupervisor do
-  use Supervisor
+  @moduledoc """
+  `Livestream.Room.RoomSupervisor`
+  """
+  use DynamicSupervisor
   alias Livestream.Room
 
   @doc false
-  def init(_opts), do: Supervisor.init([Room], strategy: :simple_one_for_one)
+  def start_link(opts) do
+    DynamicSupervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
 
   @doc false
-  def start_link(_opts) do
-    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  def init(_opts), do: Supervisor.init(strategy: :one_for_one)
+
+  @doc """
+  add/1 creates a new room
+  """
+  def add(opts) do
+    spec = %{
+      id: "room-#{:os.system_time()}",
+      type: :worker,
+      start: {Room, :start_link, [opts]},
+      restart: :temporary
+    }
+
+    DynamicSupervisor.start_child(__MODULE__, spec)
   end
 end
